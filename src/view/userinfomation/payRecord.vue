715bc787b4d8a51ec1162602fa19c1d9f9fade34
BadmanY <zhangyang1103@stu.cdut.edu.cn>
2023-07-07 14:35:42 +0800

    记录

--- src/view/userinfomation/payRecord.vue
+++ src/view/userinfomation/payRecord.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-
+        <div class="self">
+        <div style="width: 100%;height: 10vh; ">
+            <h2 style="font-size: 30px;"><i class="el-icon-search"></i>充值记录</h2>
+            <el-divider/>
+        </div>
     </div>
 </template>
 

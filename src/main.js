1b8dbb0df67e672a7938de630f022f4f72e16069
kelf <kivenf3776@gmail.com>
2023-07-08 21:12:17 +0800

    修改bug

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router/index'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
-import * as echarts from 'echarts';
 axios.defaults.baseURL='http://127.0.0.1:3000'
 import axios from 'axios';
 
